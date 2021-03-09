666729a9514ec1b9b592ad365a31659eeebbbd5a
Home <Home@LAPTOP-2E6V6ACM>
2021-03-09 20:27:04 +0200

    Add project files.

---
+++ LayoutsDemo/DemoAbsoluteLayout.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutsDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DemoAbsoluteLayout : ContentPage
+    {
+        public DemoAbsoluteLayout()
+        {
+            InitializeComponent();
+        }
+
+        bool animate;
+
+        
+        protected  override void OnAppearing()
+        {
+            base.OnAppearing();
+            animate = false;
+            chkStart.IsChecked = false;
+            UpdatePos();
+        }
+
+        private void UpdatePos()
+        {
+            Rectangle r1 = AbsoluteLayout.GetLayoutBounds(text1);
+            Rectangle r2 = AbsoluteLayout.GetLayoutBounds(text2);
+            txtPos.Text = $"T1:{Math.Round(r1.Top,2)}x{Math.Round(r1.Left,2)}, T2:{Math.Round(r2.Top, 2)}x{Math.Round(r2.Left, 2)}";
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            animate = false;
+            chkStart.IsChecked = false;
+        }
+
+        private void chkStart_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            animate = chkStart.IsChecked;
+
+            if (animate)
+            {
+                DateTime beginTime = DateTime.Now;
+
+                Device.StartTimer(TimeSpan.FromSeconds(1.0 / 30), () =>
+                {
+                    double seconds = (DateTime.Now - beginTime).TotalSeconds;
+                    double offset = 1 - Math.Abs((seconds % 2) - 1);
+
+                    AbsoluteLayout.SetLayoutBounds(text1,
+                        new Rectangle(offset, offset,
+                            AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+                    AbsoluteLayout.SetLayoutBounds(text2,
+                        new Rectangle(1 - offset, offset,
+                            AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+                    UpdatePos();
+                    return animate;
+                });
+            }
+        }
+    }
+}
---
+++ LayoutsDemo/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutsDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnContentView_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoContentView());
+        }
+
+        private async void btnFrame_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoFrame());
+        }
+
+        private async void btnScrollView_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoScrollView());
+        }
+
+        private async void btnStackLayout_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoStackLayout());
+        }
+
+        private async void btnGrid_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoGrid());
+        }
+
+        private async void btnAbsolutLayout_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoAbsoluteLayout());
+        }
+
+        private async void btnRelativeLayout_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DemoRelativeLayout());
+        }
+    }
+}
